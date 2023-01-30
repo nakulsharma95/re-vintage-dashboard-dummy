98f527ebc9593e4a1de93d1012f2db1ff8156129
unknown <jateen.sunder@techchefz.com>
2023-01-30 13:24:59 +0530

    UI

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec88fa87f93c7b7a"; }
+/******/ 		__webpack_require__.h = function() { return "31fd0a5a1b5c34f7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/procurement/index.jsx
@@ -0,0 +1,12 @@
+import style from './style.module.scss';
+
+export default function Procurement() {
+  return (
+    <div className={style.ProcurementStyle}>
+      <h1 className={`${style.titleCustomStyle} title2`}>
+        Welcome to 😊
+        <br /> Next JS Boiler plate
+      </h1>
+    </div>
+  );
+}
