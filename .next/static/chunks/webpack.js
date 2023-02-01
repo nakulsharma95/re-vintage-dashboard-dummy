b3b1e3e201a2fe9bc5d1fa68517924626f14f63f
Rohit Ahlawat <rohit.ahlawat@techchefz.com>
2023-02-01 15:54:09 +0530

    bike card bread crumb and filter ui created

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ea7a6e348816aaa8"; }
+/******/ 		__webpack_require__.h = function() { return "bf665da6bec321a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
