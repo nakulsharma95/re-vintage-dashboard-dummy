095c4a0f98be82bb8b6c46967cb2f3c7f4bb1eec
Alok Kumar <alok.kumar@techchefz.com>
2023-02-15 19:23:36 +0530

    fix: all card maping

--- components/procurements/detailCard/index.jsx
+++ components/procurements/detailCard/index.jsx
@@ -6,87 +6,71 @@ import styles from './style.module.scss';
 import ArrowButton from '../../common/buttons/ArrowButton';
 
 export default function DetailCard(props) {
-  const cardData = [
-    {
-      ownerName: 'Rishab Sharma',
-    },
-    {
-      ownerName: 'Rishab kumar',
-    },
-    {
-      ownerName: 'Rishab Sharma',
-    },
-  ];
+  
   return (
     <>
-      {cardData.map((item) => (
-        <Col xl={4} md={6}>
-          <div className="mb-4">
-            <Card className={styles.detailCard}>
-              <Card.Body>
-                <div
-                  className={`${styles.headerDetail} ${styles.detailsDivide} p-0 border-0 `}
-                >
-                  <h2 className="text-white mb-0">
-                    Details{' '}
-                    <span className="text-white-50">-FB29FH9219HR1</span>
-                  </h2>
-                  <div
-                    className={`${styles.detailDrop} ${styles.detailsDivide} border-0 shadow-none text-white  gap-1 }`}
-                  >
-                    <AiOutlineClose size={18} />
-                    <span className="mb-0">Drop Lead</span>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <div
-                    className={`${styles.borderStyles} ${styles.detailsDivide}   pb-2`}
-                  >
-                    <div className={`${styles.detailName}`}>
-                      <span className="text-white-50">Owner Name</span>
-                      <p className="mb-0 text-white ">{item.ownerName}</p>
-                    </div>
-                    <div className={`${styles.detailContact}`}>
-                      <span className="text-white-50">Contact No.</span>
-                      <p className="mb-0 text-white ">+91 9876543219</p>
-                    </div>
-                  </div>
-                  <div
-                    className={`${styles.borderStyles} ${styles.detailsDivide}  pb-2 mt-1`}
-                  >
-                    <div className={`${styles.detailEmail}`}>
-                      <span className="text-white-50">Email</span>
-                      <p className="mb-0 text-white ">john@gmail.com</p>
-                    </div>
-                    <div className={`${styles.detailPincode}`}>
-                      <span className="text-white-50">Pincode</span>
-                      <p className="mb-0 text-white ">110014</p>
-                    </div>
-                  </div>
-
-                  <div className={`${styles.detailsDivide} py-2 `}>
-                    <ArrowButton detailLink="/" title="Contact Customer" />
-                    <div className={`${styles.detailSendForm} text-white`}>
-                      <FaRegClipboard className="mx-1" size={16} />
-                      <span>Send eval. form</span>
-                    </div>
-                  </div>
-                </div>
-              </Card.Body>
-              <div
-                className={`${styles.detailCardFooter} ${styles.detailsDivide}  card-footer bg-dark`}
-              >
-                <p className="text-white-50 mb-0">
-                  Prm source: <span className="text-white">MIY</span>
-                </p>
-                <p className="text-white-50 mb-0">
-                  Sec source: <span className="text-white">Google</span>
-                </p>
-              </div>
-            </Card>
+      <Card className={styles.detailCard}>
+        <Card.Body>
+          <div
+            className={`${styles.headerDetail} ${styles.detailsDivide} p-0 border-0 `}
+          >
+            <h2 className="text-white mb-0">
+              Details{' '}
+              <span className="text-white-50">-{props.detailNumber}</span>
+            </h2>
+            <div
+              className={`${styles.detailDrop} ${styles.detailsDivide} border-0 shadow-none text-white  gap-1 }`}
+            >
+              <AiOutlineClose size={18} />
+              <span className="mb-0">Drop Lead</span>
+            </div>
           </div>
-        </Col>
-      ))}
+          <div className="mt-2">
+            <div
+              className={`${styles.borderStyles} ${styles.detailsDivide}   pb-2`}
+            >
+              <div className={`${styles.detailName}`}>
+                <span className="text-white-50">Owner Name</span>
+                <p className="mb-0 text-white ">{props.ownerName}</p>
+              </div>
+              <div className={`${styles.detailContact}`}>
+                <span className="text-white-50">Contact No.</span>
+                <p className="mb-0 text-white ">+91 9876543219</p>
+              </div>
+            </div>
+            <div
+              className={`${styles.borderStyles} ${styles.detailsDivide}  pb-2 mt-1`}
+            >
+              <div className={`${styles.detailEmail}`}>
+                <span className="text-white-50">Email</span>
+                <p className="mb-0 text-white ">john@gmail.com</p>
+              </div>
+              <div className={`${styles.detailPincode}`}>
+                <span className="text-white-50">Pincode</span>
+                <p className="mb-0 text-white ">110014</p>
+              </div>
+            </div>
+
+            <div className={`${styles.detailsDivide} py-2 `}>
+              <ArrowButton detailLink="/" title="Contact Customer" />
+              <div className={`${styles.detailSendForm} text-white`}>
+                <FaRegClipboard className="mx-1" size={16} />
+                <span>Send eval. form</span>
+              </div>
+            </div>
+          </div>
+        </Card.Body>
+        <div
+          className={`${styles.detailCardFooter} ${styles.detailsDivide}  card-footer bg-dark`}
+        >
+          <p className="text-white-50 mb-0">
+            Prm source: <span className="text-white">MIY</span>
+          </p>
+          <p className="text-white-50 mb-0">
+            Sec source: <span className="text-white">Google</span>
+          </p>
+        </div>
+      </Card>
       <Col md={12}>
         {props.detailCardBtmTxt && (
           <p className={styles.detailCardTxt}>{props.detailCardBtmTxt}</p>
--- pages/procurement/index.jsx
+++ pages/procurement/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import BikeCard from '../../components/procurements/bikeCard';
 import Breadcrumb from '../../components/common/breadcrumbPrimary';
 import DetailCard from '../../components/procurements/detailCard';
@@ -7,14 +7,34 @@ import ViewAll from '../../components/common/viewAllHeader';
 import Filters from '../../components/common/filters';
 
 function Procurement() {
+  const cardData = [
+    {
+      ownerName: 'Rishab Sharma',
+      detailNo: '123456',
+    },
+    {
+      ownerName: 'Rishab kumar',
+      detailNo: '123456',
+    },
+    {
+      ownerName: 'Rishab Sharma',
+      detailNo: '123456',
+    },
+  ];
   return (
     <div>
       <Breadcrumb title="Procurement" addmoretitle="Procurement" />
       <Filters title="Procurement Leads" />
       <div className="bottom-white-border" />
+      <ViewAll title="Partial Leads" leadsTitle="(8 Leads)" />
       <Row>
-        <ViewAll title="Partial Leads" leadsTitle="(8 Leads)" />
-        <DetailCard />
+        {cardData.map((item) => (
+          <Col xl={4} md={6}>
+            <DetailCard
+              detailNumber={item.detailNo}
+            />
+          </Col>
+        ))}
       </Row>
       <Row>
         <ViewAll title="Evaluation Requests" leadsTitle="(8 Leads)" />
