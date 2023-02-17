0aa5920d78616453dd38b80812b593165471e8b6
sachinsharma92 <sachindesigner2018@gmail.com>
2023-02-17 18:02:59 +0530

    FIX: some major chart issues

--- components/charts/pieChart/index.jsx
+++ components/charts/pieChart/index.jsx
@@ -1,44 +1,16 @@
 import Chart from 'react-google-charts';
+// import styles from './style.module.scss';
 
-function PieChart() {
-  const pieOptions = {
-    title: '',
-    backgroundColor: '',
-    pieHole: 0.5,
-    slices: [
-      {
-        color: '#D2635B',
-      },
-      {
-        color: '#F1DF67',
-      },
-    ],
-    legend: {
-      position: 'bottom',
-      alignment: 'left',
-      textStyle: {
-        color: 'white',
-        fontSize: 14,
-      },
-    },
-    tooltip: {
-      showColorCode: true,
-    },
-    fontSize: 20,
-  };
-
+function PieChart(props) {
   return (
     <Chart
       chartType="PieChart"
-      data={[
-        ['Item', 'Numbers'],
-        ['Item 2', 10000],
-        ['Item 3', 6000],
-      ]}
-      options={pieOptions}
+      data={props.chartData}
+      options={props.pieOptions}
       width="100%"
       height="220px"
       legend_toggle
+      loader={<div>Loading Chart...</div>}
     />
   );
 }
