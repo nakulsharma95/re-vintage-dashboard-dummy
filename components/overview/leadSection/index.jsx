18ba5262a0e5ec16ed3437fd10d7d195f449ec21
Disha Jain <disha.jain@techchefz.com>
2023-02-20 17:43:42 +0530

    fix: overview ui

--- components/overview/leadSection/index.jsx
+++ components/overview/leadSection/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table';
-import Link from 'next/link';
-import { AiOutlineDownload } from 'react-icons/ai';
 import PieBig from '../pieBig';
 import PieSmall from '../pieSmall';
 import styles from './style.module.scss';
+import ProcurementTable from '../procurementTable';
+import DealClouser from '../dealClouser';
 
 function LeadSection() {
   return (
@@ -12,117 +11,13 @@ function LeadSection() {
       <div className={styles.leadsTop}>
         <PieSmall withouticon />
         <div className={`${styles.leadTable}  dashboardTable`}>
-          <Table striped variant="dark">
-            <tbody>
-              <tr>
-                <td className={styles.BoldHead}>
-                  Procurement
-                  <AiOutlineDownload className={styles.BoldHeadIcon} />
-                </td>
-                <td>Open</td>
-                <td>Cancelled</td>
-                <td>Dropped</td>
-                <td>Total</td>
-                <td>Action</td>
-              </tr>
-              <tr>
-                <td>Pratial Lead</td>
-                <td>5</td>
-                <td>NA</td>
-                <td>6</td>
-                <td>7</td>
-                <td>
-                  <Link href="/">View all</Link>
-                </td>
-              </tr>
-              <tr>
-                <td>Pratial Lead</td>
-                <td>5</td>
-                <td>NA</td>
-                <td>6</td>
-                <td>7</td>
-                <td>
-                  <Link href="/">View all</Link>
-                </td>
-              </tr>
-              <tr>
-                <td>Pratial Lead</td>
-                <td>5</td>
-                <td>NA</td>
-                <td>6</td>
-                <td>7</td>
-                <td>
-                  <Link href="/">View all</Link>
-                </td>
-              </tr>
-              <tr>
-                <td>Pratial Lead</td>
-                <td>5</td>
-                <td>NA</td>
-                <td>6</td>
-                <td>7</td>
-                <td>
-                  <Link href="/">View all</Link>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
+        <ProcurementTable />
         </div>
       </div>
       <div className={styles.leadsMiddle}>
         <div className={`${styles.leadMidTable} dashboardTable leadTable`}>
-          <Table striped bordered variant="dark">
-            <thead>
-              <tr>
-                <th rowSpan={2}>Deal Clouser</th>
-                <th colSpan={2}>Dealer</th>
-                <th colSpan={2}>Sealler</th>
-              </tr>
-              <tr>
-                <th>open</th>
-                <th>Droped</th>
-                <th>open</th>
-                <th>Draoped</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Price Acceptance</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
-              <tr>
-                <td>Price Acceptance</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
-              <tr>
-                <td>Price Acceptance</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
-              <tr>
-                <td>Price Acceptance</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
-              <tr>
-                <td>Price Acceptance</td>
-                <td>-</td>
-                <td>Na</td>
-                <td>-</td>
-                <td>Na</td>
-              </tr>
-            </tbody>
-          </Table>
+          <DealClouser/>
+          
         </div>
 
         <PieSmall icon />
