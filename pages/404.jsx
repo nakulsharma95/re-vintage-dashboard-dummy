3a444c1b31b93d2c938a14e37b1d628217c54ebc
Alok Kumar <alok.kumar@techchefz.com>
2023-02-22 15:53:53 +0530

    fix: add arrow icon in review slider in dealer management detail page

--- pages/404.jsx
+++ pages/404.jsx
@@ -9,11 +9,10 @@ export default function FourOhFour() {
       <div className="errorMain">
         <div className="errorContainer">
           <div className="errorImg">
-            <Image src="/images/error.jpg" alt="not found" />
+            <Image src="/images/error.png" alt="not found" />
           </div>
           <h3>Whoops! We Could Not Find The Page You're Looking For.</h3>
-          <p>Maybe you were typing to fast. Or maybe we moved the page you were looking for (sorry about that). Whatever happened, there's
-            nothing to see here, Try one of the links below to get back on track</p>
+          <p>Maybe you were typing too fast. Or maybe we moved the page you were looking for (sorry about that). Whatever happened, there's nothing to see here, Try one of the links below to get back on track.</p>
 
             <Link href="/">Go Back</Link>
         </div>
