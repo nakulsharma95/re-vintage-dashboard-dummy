635edb447e5e6deddb9356024e79a40778f72259
Jateen Sunder <jateen.sunder@techchefz.com>
2023-03-01 16:12:12 +0530

    fix: woking on ui responsive feedback

--- pages/deal-closure/procured/index.jsx
+++ pages/deal-closure/procured/index.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Row, Col, Tabs, Tab } from 'react-bootstrap';
-import { RiShareBoxFill } from 'react-icons/ri';
-import CloserBikeCard from '../../../components/dealClosure/closerCard';
-import Breadcrumb from '../../../components/common/breadcrumbPrimary';
-import DealFilter from '../../../components/dealClosure/dealFilter';
-import SearchPrimary from '../../../components/common/searchPrimary';
-import FilterButton from '../../../components/common/filterButton';
-import HeaderPrimary from '../../../components/common/headerPrimary';
-import OutlineButton from '../../../components/common/buttons/OutlineButton';
+import React from "react";
+import { Row, Col, Tabs, Tab } from "react-bootstrap";
+import { RiShareBoxFill } from "react-icons/ri";
+import CloserBikeCard from "../../../components/dealClosure/closerCard";
+import Breadcrumb from "../../../components/common/breadcrumbPrimary";
+import SearchPrimary from "../../../components/common/searchPrimary";
+import FilterButton from "../../../components/common/filterButton";
+import HeaderPrimary from "../../../components/common/headerPrimary";
+import OutlineButton from "../../../components/common/buttons/OutlineButton";
+import SortTabs from "~/components/common/sortTabs";
 
 function PaymentsPending() {
   const cardData = [
     {
-      imageUrl: '/images/bikeImage.png',
-      overDue: 'evolutionOver',
-      bikeStatus: 'Upload Content',
+      imageUrl: "/images/bikeImage.png",
+      overDue: "evolutionOver",
+      bikeStatus: "Upload Content",
     },
     {
-      imageUrl: '/images/bikeImage2.png',
-      bikeStatus: 'Under Refurbishment',
+      imageUrl: "/images/bikeImage2.png",
+      bikeStatus: "Under Refurbishment",
     },
     {
-      imageUrl: '/images/bikeImage3.png',
-      bikeStatus: 'Under Approval',
+      imageUrl: "/images/bikeImage3.png",
+      bikeStatus: "Under Approval",
     },
     {
-      imageUrl: '/images/bikeImage4.png',
+      imageUrl: "/images/bikeImage4.png",
     },
   ];
   return (
@@ -42,25 +42,42 @@ function PaymentsPending() {
           defaultActiveKey="title3"
           id="uncontrolled-tab-example"
         >
-          <Tab eventKey="title1" title="Dealer (25)" />
-          <Tab eventKey="title2" title="Seller Portal (12)" />
+          <Tab eventKey="title1" title="Dealer (25)">
+            <Row>
+              <SortTabs selectAllCheck sortTabTitle="8 Leads" />
+              {cardData.map((item) => (
+                <Col xxl={3} xl={4} lg={6} md={6}>
+                  <CloserBikeCard
+                    data={item}
+                    isHighestBid="Evolution Price"
+                    isKmDriven
+                    dealPrice
+                    cardTag
+                    location
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Tab>
+          <Tab eventKey="title2" title="Seller Portal (12)">
+            <Row>
+              <SortTabs selectAllCheck sortTabTitle="8 Leads" />
+              {cardData.map((item) => (
+                <Col xxl={3} xl={4} lg={6} md={6}>
+                  <CloserBikeCard
+                    data={item}
+                    isHighestBid="Evolution Price"
+                    isKmDriven
+                    dealPrice
+                    cardTag
+                    location
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Tab>
         </Tabs>
       </div>
-      <DealFilter title="8 Leads" />
-      <Row>
-        {cardData.map((item) => (
-          <Col xxl={3} xl={4} lg={6} md={6}>
-            <CloserBikeCard
-              data={item}
-              isHighestBid="Evolution Price"
-              isKmDriven
-              dealPrice
-              cardTag
-              location
-            />
-          </Col>
-        ))}
-      </Row>
     </>
   );
 }
