73fb560cf058a8be074c477a96373187ed99cadf
sachinsharma92 <sachindesigner2018@gmail.com>
2023-03-09 11:19:49 +0530

    FIX: some issues

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import { SSRProvider } from 'react-bootstrap';
+import { Provider } from 'react-redux';
 import Layout from '../components/common/layout';
 import store from '../redux/store';
 
@@ -6,19 +6,19 @@ import store from '../redux/store';
 import 'react-calendar/dist/Calendar.css';
 
 // Slick Carousel CSS
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 // Styles here
 import '../styles/theme.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SSRProvider store={store}>
+    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </SSRProvider>
+    </Provider>
   );
 }
 
